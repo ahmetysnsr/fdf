728c423231750d6bc9ebeca418c23cb3695ab18b
ahmetysnsr <44652123+ahmetysnsr@users.noreply.github.com>
2026-07-14 03:51:34 +0300

    Add remaining source files

---
+++ utils1.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asari <asari@student.42.fr>                +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*                                                +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*   Created: 2025/12/02 18:39:19 by asari             +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*   Updated: 2025/12/05 03:48:38 by asari            +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+#include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\n' || c == '\t' || c == '\v'
+		|| c == '\f' || c == '\r');
+}
+
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	int		result;
+
+	sign = 1;
+	result = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str++ - 48);
+	}
+	return (result * sign);
+}
+
+int	ft_atoi_hex(char *str)
+{
+	int	result;
+	int	i;
+
+	result = 0;
+	i = 0;
+	if (!str)
+		return (0);
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '0' && (str[i + 1] == 'x' || str[i + 1] == 'X'))
+		i += 2;
+	while (str[i])
+	{
+		result = result * 16;
+		if (str[i] >= '0' && str[i] <= '9')
+			result += str[i] - '0';
+		else if (str[i] >= 'a' && str[i] <= 'f')
+			result += str[i] - 'a' + 10;
+		else if (str[i] >= 'A' && str[i] <= 'F')
+			result += str[i] - 'A' + 10;
+		else
+			break ;
+		i++;
+	}
+	return (result);
+}
+
+void	free_matrix(t_map *map)
+{
+	int	y;
+
+	if (!map->matrix)
+		return ;
+	y = 0;
+	while (y < map->height)
+	{
+		if (map->matrix[y])
+			free(map->matrix[y]);
+		y++;
+	}
+	free(map->matrix);
+	map->matrix = NULL;
+}
---
+++ utils2.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asari <asari@student.42.fr>                +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*                                                +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*   Created: 2025/12/02 20:51:28 by asari             +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*   Updated: 2025/12/05 03:26:54 by asari            +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "fdf.h"
+#include "GNL/get_next_line.h"
+
+void	flush_gnl(int fd)
+{
+	char	*temp;
+
+	temp = get_next_line(fd);
+	while (temp)
+	{
+		free(temp);
+		temp = get_next_line(fd);
+	}
+}
+
+int	check_extension(char *filename)
+{
+	int	len;
+
+	len = 0;
+	while (filename[len])
+		len++;
+	if (len < 4)
+		return (0);
+	if (filename[len - 4] == '.' && filename[len - 3] == 'f'
+		&& filename[len - 2] == 'd' && filename[len - 1] == 'f')
+		return (1);
+	return (0);
+}
+
+int	ft_ternary(int condition, int true_val, int false_val)
+{
+	if (condition)
+		return (true_val);
+	return (false_val);
+}
+
+void	compute_view(t_map *map, t_view *v)
+{
+	v->win_w = 1200;
+	v->win_h = 800;
+	v->scale = cal_scale(map, v);
+	v->zscale = 0.5f;
+	v->angle = 0.523599f;
+	v->offset_x = v->win_w / 2;
+	v->offset_y = v->win_h / 2 - (map->height * v->scale) / 2;
+}
